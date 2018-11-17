36ec31ce9ad82b70dc8fea3a3400c0ddb5c56199
Carson Holzheimer <carson@corp.redeye.co>
2018-11-17 14:18:28 +1000

    add splash screen and home screen

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() {
+    return new SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/splashScreen.jpg',
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
